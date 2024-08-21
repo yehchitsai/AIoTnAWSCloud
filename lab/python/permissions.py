1b42e5936e20fd2ee9517949be7dab68abc1ba84
Chi-Tsai Yeh <yehchitsai@gmail.com>
2024-08-21 11:20:47 +0800

    fix: get current path for executing script

--- lab/python/permissions.py
+++ lab/python/permissions.py
@@ -1,11 +1,11 @@
 import boto3
 import json
+import pathlib
 
 s3_client = boto3.client("s3", region_name="us-east-1")
 bucket_name = "<bucket-name>"
-
-policy_file = open("s3_security_policy.json", "r")
-
+current_path = str(pathlib.Path(__file__).parent.resolve())
+policy_file = open(current_path + "s3_security_policy.json", "r")
 
 s3_client.put_bucket_policy(
     Bucket = bucket_name,
